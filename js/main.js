5d04d95d1233140ae7bd7d34c2a03b7d6af5361e
sahosisocial <sahos@isocial.com.bd>
2022-07-03 13:12:56 +0600

    responsive done width out table

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
     $(".navbar-nav").toggle();
   });
 
+  $(".top_menu_bar").click(function () {
+    $(".exta_btn").toggleClass("d-block", 300);
+  });
+
   $(".sidebar").mouseout(function () {
     $(".tb_title").addClass("tb_title_hide", 300);
     $(".sidebar_text").addClass("sidebar_text_hide", 300);
